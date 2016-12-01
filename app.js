9a4177150d2ad76e76cb2706cf20fcef5486725b
Msatkin <mscottaa@gmail.com>
2016-12-01 17:32:46 -0600

    testing

--- app.js
+++ app.js
@@ -278,12 +278,11 @@ printAllToConsole(dataObject);
 
 function initSearch(){
 	alert("Hello World");
-
 	// get all the information you need to run the search
 	var yourName = prompt("Who do you want to search for?");
 
 	// then pass that info to the respective function.
-	var result = getPersonInfo("J", "T")
+	var result = getPersonInfo("Joey", "Madden");
 
 	// once the search is done, pass the results to the responder function
 	responder(result);
@@ -295,8 +294,7 @@ function responder(results){
 }
 
 function getPersonInfo(firstname, lastname){
-	var result = "This will be the information for whoever you searched for";
-	// look up person's information
+	var result = dataObject[0];
 	return result;
 }
 
@@ -307,4 +305,4 @@ function getFamily(){
 // there will be much more here, and some of the code above will certainly change
 
 initSearch();
-window.close(); // exit window as the end of the session -- you may remove this
+//window.close(); // exit window as the end of the session -- you may remove this
