743f9310a98e01803802c4513e8d21ddcb682b01
Msatkin <mscottaa@gmail.com>
2016-12-07 16:36:02 -0600

    fixed get next of kin issues

--- app-spec.js
+++ app-spec.js
@@ -24,32 +24,29 @@ describe('function getOldest', function() {
 
 describe('function getNextOfKin', function() {
     it('get next of kin', function() {
-        expect(getNextOfKin(21, getDob, getOldest, getChildren, getSpouse, getParents, getSiblings, getDoubleRelation, getTripleRelation, getIndexFromId)).toEqual(19);
+        expect(getNextOfKin(21, getDob, getOldest, getChildren, getSpouse, getParents, getSiblings, getDoubleRelation, getTripleRelation, getIndexFromId)).toEqual('19');
     });
     it('get next of kin', function() {
-        expect(getNextOfKin(20, getDob, getOldest, getChildren, getSpouse, getParents, getSiblings, getDoubleRelation, getTripleRelation, getIndexFromId)).toEqual(19);
+        expect(getNextOfKin(20, getDob, getOldest, getChildren, getSpouse, getParents, getSiblings, getDoubleRelation, getTripleRelation, getIndexFromId)).toEqual('19');
     });
     it('get next of kin', function() {
-        expect(getNextOfKin(18, getDob, getOldest, getChildren, getSpouse, getParents, getSiblings, getDoubleRelation, getTripleRelation, getIndexFromId)).toEqual(17);
-    });
-    it('get next of kin', function() {
-        expect(getNextOfKin(9, getDob, getOldest, getChildren, getSpouse, getParents, getSiblings, getDoubleRelation, getTripleRelation, getIndexFromId)).toEqual(17);
+        expect(getNextOfKin(9, getDob, getOldest, getChildren, getSpouse, getParents, getSiblings, getDoubleRelation, getTripleRelation, getIndexFromId)).toEqual('17');
     });
 });
 
-describe('function getChildren', function(){
-  it('confirm Joey Madden has no children', function(){
-    expect(getChildren(21)).toEqual([]);
-  });
-  it('confirm Uma Bob has children', function(){
-    expect(getChildren(1)).toEqual(['10', '11']);
-  });
+describe('function getChildren', function() {
+    it('confirm Joey Madden has no children', function() {
+        expect(getChildren(21)).toEqual([]);
+    });
+    it('confirm Uma Bob has children', function() {
+        expect(getChildren(1)).toEqual(['10', '11']);
+    });
 });
 
-describe('function getSiblings', function(){
-  it('confirm Joey Madden is an only child', function(){
-    expect(getSiblings(21, getParents, getChildren)).toEqual([]);
-  });
+describe('function getSiblings', function() {
+    it('confirm Joey Madden is an only child', function() {
+        expect(getSiblings(21, getParents, getChildren)).toEqual([]);
+    });
 });
 
 /*describe('function estimateAge', function(){
